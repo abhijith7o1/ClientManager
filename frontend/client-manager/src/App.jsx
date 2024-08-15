d24761a90404f7d6cd7d9725380ec0012e0da8e6
abhijith <abhijith.official7o1@gmail.com>
2024-08-15 01:45:59 -0800

    chore(initial): initial code

---
+++ frontend/client-manager/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        <span>Home</span>
+        <span>Student</span>
+        <span>Facaulty</span>
+        <span>Assets</span>
+        <span>Logout</span>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
